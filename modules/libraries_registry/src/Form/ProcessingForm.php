39d4dd13c3b6bca0f4e55ad02e6a43176d7c7d57
Ryan Jacobs <rjacobs@422459.no-reply.drupal.org>
2016-09-12 23:05:18 -0500

    Add basic version detection conversion for D7 structures that can be mapped to known D8 plugins.

--- modules/libraries_registry/src/Form/ProcessingForm.php
+++ modules/libraries_registry/src/Form/ProcessingForm.php
@@ -63,11 +63,11 @@ class ProcessingForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['actions']['#type'] = 'actions';
-    $form['actions']['submit'] = array(
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Process'),
       '#button_type' => 'primary',
-    );
+    ];
     return $form;
   }
 
@@ -81,8 +81,6 @@ class ProcessingForm extends FormBase {
     $saved_count = 0;
     foreach ($libraries_info as $library_name => $data) {
       $this->preSerializeAlter($library_name, $data);
-      // @todo: Switch to YAML
-      // @see https://www.drupal.org/node/1897612
       $serialized = $this->serializer->serialize($data, 'json', ['json_encode_options' => JSON_PRETTY_PRINT]);
       if ($serialized) {
         $this->postSerializeAlter($library_name, $serialized);
@@ -107,7 +105,7 @@ class ProcessingForm extends FormBase {
     // Get an id based on library name. We'll also use this as the raw filename.
     $id = strtolower(preg_replace('/[^0-9a-zA-Z]/', '_', $library_name));
     $library_name = $id;
-    // Add a type to the data (assume an asset library for now).
+    // Add a type to the TOP of the data (assume an asset library for now).
     $library_data = array('type' => 'asset') + $library_data;
     // Callbacks are likely no longer relevant.
     unset($library_data['callbacks']);
@@ -120,10 +118,12 @@ class ProcessingForm extends FormBase {
         $this->convertFiles($library_name, $variant_data);
       }
     }
+    // Process version detection details.
+    $this->convertVersionDef($library_name, $library_data);
   }
 
   /**
-   * Convert D7 files array to D8 structures.
+   * Utility: convert D7 files array to D8 structures.
    *
    * @param string $library_name
    *   The machine name of the library.
@@ -131,11 +131,11 @@ class ProcessingForm extends FormBase {
    *   A library definition array element to test for files data and convert if
    *   needed.
    */
-  protected function convertFiles(&$library_name, array &$element) {
+  private function convertFiles($library_name, array &$element) {
     if (!empty($element['files'])) {
       // Process CSS definitions.
       if (!empty($element['files']['css'])) {
-        $element['css']['base'] = array(); // Assume base for modules
+        $element['css']['base'] = []; // Assume base for modules
         foreach ($element['files']['css'] as $file => $data) {
           $element['css']['base'][$file] = $data;
         }
@@ -146,6 +146,53 @@ class ProcessingForm extends FormBase {
       }
       unset($element['files']);
     }
+  }
+
+  /**
+   * Utility: Convert D7 version def info to D8 structures.
+   *
+   * @param string $library_name
+   *   The machine name of the library.
+   * @param array $library_data
+   *   An array of library data as fetced from hook_libraries_info to be
+   *   altered.
+   */
+  private function convertVersionDef($library_name, array &$library_data) {
+    // Detect line pattern version config.
+    if (!empty($library_data['version arguments']['pattern'])) {
+      $library_data['version_detector'] = [
+        'id' => 'line_pattern',
+        'configuration' => $library_data['version arguments'],
+      ];
+      // "cols" key has changed to "columns"
+      if (!empty($library_data['version_detector']['configuration']['cols'])) {
+        $library_data['version_detector']['configuration']['columns'] = $library_data['version_detector']['configuration']['cols'];
+        unset($library_data['version_detector']['configuration']['cols']);
+      }
+    }
+    // Handle some version callbacks that can be programmatically mapped to
+    // known D8 detection plugins.
+    elseif (!empty($library_data['version callback']) && !empty($library_data['version arguments'])) {
+      switch ($library_data['version callback']) {
+        // Library pack uses a callback to force a static version.
+        case '_library_pack_force_version':
+          $library_data['version_detector'] = [
+            'id' => 'static',
+            'configuration' => [
+              'version' => $library_data['version arguments']['force'],
+            ],
+          ];
+          break;
+        default:
+          drupal_set_message(t('@lib: A version callback was specified but the implmenting logic could not be progrmatically converted.', ['@lib' => $library_name]), 'error');
+      }
+    }
+    else {
+      drupal_set_message(t('@lib: Version detection details could not be determined.', ['@lib' => $library_name]), 'error');
+    }
+    // Cleanup the old structures that have no meaning in D8.
+    unset($library_data['version callback']);
+    unset($library_data['version arguments']);
   }
 
   /**
